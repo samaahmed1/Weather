b606185f07b71369f548aeced83cbd0ed3b12a05
samaahmed1 <sama34777@gmail.com>
2025-03-05 17:12:47 +0200

    Add: Home Page

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home (){
+  return(
+    <section className="py-20">
+    <div className="container px-5 mx-auto flex flex-col justify-center items-center gap-7">
+    <span className="text-3xl text-white font-bold">No Data Available</span>
+    <img className="w-44 rounded-xl" src="/src/images/weathe.webp" alt="weathe" />
+    </div>
+    </section>
+  )
+}
