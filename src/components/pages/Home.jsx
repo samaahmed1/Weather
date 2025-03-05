158a5702d34be14f1b45bf7d27072a23ef4ba3fc
samaahmed1 <sama34777@gmail.com>
2025-03-05 17:24:49 +0200

    Update: Image Path in Home Page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
+import image from "../../images/weathe.webp"
 export default function Home (){
   return(
     <section className="py-20">
     <div className="container px-5 mx-auto flex flex-col justify-center items-center gap-7">
     <span className="text-3xl text-white font-bold">No Data Available</span>
-    <img className="w-44 rounded-xl" src="/src/images/weathe.webp" alt="weathe" />
+    <img className="w-44 rounded-xl" src={image} alt="weathe" />
     </div>
     </section>
   )
