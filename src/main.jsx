ec4679da24ef5bd5856c573cf38d7906270033ef
samaahmed1 <sama34777@gmail.com>
2025-03-05 21:35:48 +0200

    Update: Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import "./index.css";
 import App from "./App.jsx";
 import { StrictMode } from "react";
-import Header from "./layouts/Header.jsx";
 import { createRoot } from "react-dom/client";
+import Header from "./components/layouts/Header.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")).render(
