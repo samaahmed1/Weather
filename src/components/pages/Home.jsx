28b6d02114f5fdb2982d9d7e076dfe5aca615c4b
samaahmed1 <sama34777@gmail.com>
2025-03-05 21:43:35 +0200

    Fixed responsive issue in Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home (){
   return(
     <section className="py-20">
     <div className="container px-5 mx-auto flex flex-col justify-center items-center gap-7">
-    <span className="text-3xl text-white font-bold">No Data Available</span>
+    <span className="text-3xl text-white font-bold text-center">No Data Available</span>
     <img className="w-44 rounded-xl" src={image} alt="weathe" />
     </div>
     </section>
